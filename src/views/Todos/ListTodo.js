6caafd319988d012804b35bde016384419dd3c3b
Ha Thi Thuy Duong <52100542@student.tdtu.edu.vn>
2023-06-07 20:32:13 +0700

    TodoApp by React

---
+++ src/views/Todos/ListTodo.js
@@ -0,0 +1,95 @@
+import React from "react";
+import './ListTodo.scss'
+import AddTodo from "./AddTodo";
+import { toast } from 'react-toastify';
+
+
+class ListTodo extends React.Component {
+
+    state = {
+        listTodos: [
+            { id: 'todo1', title: 'Doing homework' },
+            { id: 'todo2', title: 'Making video' },
+            { id: 'todo3', title: 'Fixing bug' },
+        ],
+        editTodo: {}
+    }
+
+    addNewTodo = (todo) => {
+        this.setState({
+            listTodos: [...this.state.listTodos, todo]
+        })
+
+        toast.success("Wow so easy!")
+    }
+
+    handleDeleteTodo = (todo) => {
+        let currentTodos = this.state.listTodos;
+        currentTodos = currentTodos.filter(item => item.id !== todo.id)
+        this.setState({
+            listTodos: currentTodos
+        })
+        toast.success("Delete successed!")
+    }
+
+    handleEditTodo = (todo) => {
+        let { editTodo, listTodos } = this.state;
+        let isEmptyObj = Object.keys(editTodo).length === 0;
+        if(isEmptyObj === false && editTodo.id === todo.id){
+            let listTodosCopy = [...listTodos];
+            let objectIndex = listTodosCopy.findIndex((item => item.id === todo.id));
+            listTodosCopy[objectIndex].title = editTodo.title;
+
+            this.setState({
+                listTodos: listTodosCopy,
+                editTodo: {}
+            })
+            return;
+        }
+        this.setState({
+            editTodo: todo
+        })
+    }
+
+    handleOnChangeEditTodo = (event) => {
+        let editTodoCopy = {...this.state.editTodo};
+        editTodoCopy.title = event.target.value
+        this.setState({
+            editTodo: editTodoCopy
+        })
+    }
+
+    render() {
+        let { listTodos, editTodo } = this.state;
+        let isEmptyObj = Object.keys(editTodo).length === 0;
+
+        return (
+            <div className="list-todo-container">
+                <AddTodo addNewTodo={this.addNewTodo} />
+                <div className="list-todo-content">
+                    {listTodos && listTodos.length > 0 &&
+                        listTodos.map((item, index) => {
+                            return (
+                                <div className="todo-child" key={item.id}>
+                                    {isEmptyObj === false && item.id === editTodo.id ?
+                                        <span> {index + 1} - <input type="text" value={editTodo.title} onChange={(event) => this.handleOnChangeEditTodo(event)} /> </span>        
+                                        :
+                                        <span> {index + 1} - {item.title} </span>
+                                    }
+                                    <button className="edit" onClick={() => this.handleEditTodo(item)}>
+                                        {isEmptyObj === false && item.id === editTodo.id ? 'Save' : 'Edit'}
+                                    </button>
+                                    <button className="delete" onClick={() => this.handleDeleteTodo(item)}>Delete</button>
+                                </div>
+                            )
+                        })
+                    }
+
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ListTodo;
